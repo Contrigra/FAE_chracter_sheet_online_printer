e2f2646e891026d008933e0f16d3dce81482905f
Contrigra <gaponov60b@gmail.com>
2025-12-09 16:59:45 +0300

    feat:added inputs

---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+    const [characterData, setCharacterData] = useState({
+        id: '',
+        name: 'Misha',
+        description: '',
+        gender: '',
+        refresh: '',
+        currentPoints: '',
+        aspects: {
+            highConcept: '',
+            trouble: '',
+            aspect1: '',
+            aspect2: '',
+            aspect3: ''
+        },
+        approaches: {
+            careful: '',      // +5
+            clever: '',       // +4
+            flashy: '',        // +3
+            forceful: '',        // +2
+            quick: '',      // +1
+            sneaky: ''
+        },
+        extras: '',
+        stunts: '',
+        stress: [false, false, false],
+
+        consequences: {
+            mild2: '',
+            moderate4: '',
+            severe6: ''
+        }
+    });
+
+
+    return (
+        <div className={'main-container'}>
+            <div className={'input-panel'}>
+                <h1>Input Panel</h1>
+                <InputPanel onChange={(event) => setCharacterData(event.target.value)}/>
+            </div>
+            <div className={'display-panel'}>
+                <h1>Display Panel</h1>
+
+                <DisplayPanel characterData={characterData}/>
+            </div>
+        </div>
+    )
+}
+
+export default App
+
+// TODO Input компоненты должны модифицировать characterData
+function InputPanel() {
+    return (
+        <>
+            <IdInputBox/>
+            <AspectsInputBox/>
+            <ApproachesInputBox/>
+            <StuntsInputBox/>
+            <StressInputBox/>
+            <ConsequenceInputBox/>
+        </>
+    )
+}
+
+function IdInputBox() {
+    return (
+        <div className={"id-input-box"}>
+            <div className={"id-input-box-header input-header"}>ID</div>
+            <input type="text" id="name" name="name" placeholder={"Character Name"}/>
+            <input type="text" id="description" name="description" placeholder={"Character Description"}/>
+            <input type="text" id="refresh" name="refresh" placeholder={"Refresh"}/>
+            <input type="text" id="currentPoints" name="currentPoints" placeholder={"Current Points"}/>
+            <input type="text" id="gender" name="gender" placeholder={"Gender"}/>
+        </div>
+    )
+}
+
+function AspectsInputBox() {
+    return (
+        <div className={"aspects-input-box"}>
+            <div className={"aspects-input-box-header input-header"}>Aspects</div>
+            <input type="text" id="high-concept-aspect" name="high-concept-aspect" placeholder="High Concept"/>
+            <input type="text" id="trouble-aspect" name="trouble-aspect" placeholder="Trouble"/>
+            <input type="text" id="additional-aspect1" name="trouble-aspect"/>
+            <input type="text" id="additional-aspect2" name="additional-aspect2"/>
+            <input type="text" id="additional-aspect3" name="additional-aspect3"/>
+        </div>
+    )
+
+}
+
+function ApproachesInputBox() {
+    return (
+        <div className={"approaches-input-box"}>
+            <div className={"approaches-input-box-header input-header"}>Approaches</div>
+            <label >Careful
+                <input type="text" id="careful-approach" name="careful-approach"/>
+            </label>
+            <label >Clever
+                <input type="text" id="clever-approach" name="clever-approach"/>
+            </label>
+            <label >Flashy
+                <input type="text" id="flashy-approach" name="flashy-approach"/>
+            </label>
+            <label >Forceful
+                <input type="text" id="forceful-approach" name="forceful-approach"/>
+            </label>
+            <label >Quick
+                <input type="text" id="quick-approach" name="quick-approach"/>
+            </label>
+            <label>Sneaky
+                <input type="text" id="sneaky-approach" name="sneaky-approach"/>
+            </label>
+        </div>
+    )
+}
+
+
+
+
+function StuntsInputBox() {
+    return <div className={"stunts-input-box"}>
+        <div className={"stunts-input-box-header input-header"}>Stunts</div>
+        <input type="text" id="stunts" name="stunts"/>
+    </div>
+}
+
+function StressInputBox() {
+    return <div className={"stress-input-box"}>
+        <div className={"stress-input-box-header input-header"}>Stress</div>
+        <input type="checkbox" id="stress1" name="stress1"/>
+        <input type="checkbox" id="stress2" name="stress2"/>
+        <input type="checkbox" id="stress3" name="stress3"/>
+    </div>
+}
+
+
+function ConsequenceInputBox() {
+    return <div className={"consequence-input-box"}>
+        <div className={"consequence-input-box-header input-header"}>Consequences</div>
+        <label >2
+            <input type="text" id="consequence-input2" name="consequence-input2" placeholder="Mild"/>
+        </label>
+        <label >4
+            <input type="text" id="consequence-input4" name="consequence-input4" placeholder="Moderate"/>
+        </label>
+        <label >6
+            <input type="text" id="consequence-input6" name="consequence-input6" placeholder="Severe"/>
+        </label>
+    </div>
+}
+
+
+//  TODO сделать вывод инпута
+function DisplayPanel({characterData}) {
+    return (<div>My name is {JSON.stringify(characterData)}</div>
+    )
+}
