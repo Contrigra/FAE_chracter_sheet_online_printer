c52a0a68db6d00b30e2d902ce11f324cfae31203
Contrigra <gaponov60b@gmail.com>
2026-01-29 17:40:13 +0300

    feat: refactored change handlers to support aspects, consequences, and stunts inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 
 function App() {
     const [characterData, setCharacterData] = useState({
-        id: '',
         name: 'Misha',
         description: '',
         gender: '',
@@ -36,18 +35,21 @@ function App() {
     });
 
     function handleChange(event) {
-        // TODO mapping for aspects? Todo if event name includes consequence or aspect,
-        //  to dive into the nexted attribute and change it there for each inputBox
-        let newCharacterData = {...characterData};
 
-        if (event.target.parentElement.className === "aspects-input-box") {
+        let newCharacterData = {...characterData};
+        // TODO consequences and stunt input\state as text with new lines
+        if (event.target.dataset["aspect"]) {
             newCharacterData.aspects[event.target.name] = event.target.value;
-        } else if (event.target.parentElement.parentElement.className === "approaches-input-box") {
+
+        } else if (event.target.dataset["approach"]) {
             newCharacterData.approaches[event.target.name] = event.target.value;
 
-        } else if (event.target.parentElement.className === "stress-input-box") {
+        } else if (event.target.dataset["stress"]) {
             let targetIndex = event.target.name.at(-1) - 1;
-            newCharacterData.stress[targetIndex] = event.target.checked ;
+            newCharacterData.stress[targetIndex] = event.target.checked;
+
+        } else if (event.target.dataset["consequences"]) {
+            newCharacterData.consequences[event.target.dataset["consequences"]] = event.target.value
 
         } else {
             newCharacterData[event.target.name] = event.target.value;
@@ -109,45 +111,42 @@ function IdInputBox({onChange}) {
 }
 
 function AspectsInputBox({onChange}) {
-    // TODO Надо чтобы он нестил в аспекты.
-    // Сейчас он просто создаёт новый объект. Добавить if clause для аспектов, т.к. они занестины в хендлер?
+
     return (
         <div className={"aspects-input-box"}>
             <div className={"aspects-input-box-header input-header"}>Aspects</div>
-            <input type="text" onChange={onChange} name="highConcept"
+            <input type="text" onChange={onChange} name="highConcept" data-aspect={"highConcept"}
                    placeholder="High Concept"/>
-            <input type="text" onChange={onChange} name="trouble" placeholder="Trouble"/>
-            <input type="text" onChange={onChange} name="aspect1"/>
-            <input type="text" onChange={onChange} name="aspect2"/>
-            <input type="text" onChange={onChange} name="aspect3"/>
+            <input type="text" onChange={onChange} data-aspect={"trouble"} name="trouble" placeholder="Trouble"/>
+            <input type="text" onChange={onChange} data-aspect={"aspect1"} name="aspect1"/>
+            <input type="text" onChange={onChange} data-aspect={"aspect2"} name="aspect2"/>
+            <input type="text" onChange={onChange} data-aspect={"aspect3"} name="aspect3"/>
         </div>
     )
 
 }
 
 function ApproachesInputBox({onChange}) {
-    // TODO there's no need for ID as inputs are nested within labels.
-    //  id needs to be provided if labels use for attribute
     return (
         <div className={"approaches-input-box"}>
             <div className={"approaches-input-box-header input-header"}>Approaches</div>
             <label>Careful
-                <input type="text" onChange={onChange} name="careful"/>
+                <input type="text" onChange={onChange} name="careful" data-approach="careful"/>
             </label>
             <label>Clever
-                <input type="text" onChange={onChange} id="clever-approach" name="clever"/>
+                <input type="text" onChange={onChange} name="clever" data-approach="clever"/>
             </label>
             <label>Flashy
-                <input type="text" onChange={onChange} id="flashy-approach" name="flashy"/>
+                <input type="text" onChange={onChange} name="flashy" data-approach="flashy"/>
             </label>
             <label>Forceful
-                <input type="text" onChange={onChange} id="forceful-approach" name="forceful"/>
+                <input type="text" onChange={onChange} name="forceful" data-approach="forceful"/>
             </label>
             <label>Quick
-                <input type="text" onChange={onChange} id="quick-approach" name="quick"/>
+                <input type="text" onChange={onChange} name="quick" data-approach="quick"/>
             </label>
             <label>Sneaky
-                <input type="text" onChange={onChange} id="sneaky-approach" name="sneaky"/>
+                <input type="text" onChange={onChange} name="sneaky" data-approach="sneaky"/>
             </label>
         </div>
     )
@@ -160,33 +159,34 @@ function StuntsInputBox({onChange}) {
     // 2. Halfling luck
     return <div className={"stunts-input-box"}>
         <div className={"stunts-input-box-header input-header"}>Stunts</div>
-        <input type="text" onChange={onChange} id="stunts" name="stunts"/>
+        <textarea onChange={onChange} rows={4} name="stunts" placeholder={"1. Dangerously incompetent\n2. Halfling luck"}/>
     </div>
 }
 
 function StressInputBox({onChange}) {
     return <div className={"stress-input-box"}>
         <div className={"stress-input-box-header input-header"}>Stress</div>
-        <input type="checkbox" onChange={onChange} id="stress1" name="stress1"/>
-        <input type="checkbox" onChange={onChange} id="stress2" name="stress2"/>
-        <input type="checkbox" onChange={onChange} id="stress3" name="stress3"/>
+        <input type="checkbox" onChange={onChange} data-stress='stress1' name="stress1"/>
+        <input type="checkbox" onChange={onChange} data-stress='stress2' name="stress2"/>
+        <input type="checkbox" onChange={onChange} data-stress='stress3' name="stress3"/>
     </div>
 }
 
 
 function ConsequenceInputBox({onChange}) {
+    // TODO
     return <div className={"consequence-input-box"}>
         <div className={"consequence-input-box-header input-header"}>Consequences</div>
         <label>2
-            <input type="text" onChange={onChange} id="consequence-input2" name="consequence-input2"
+            <input type="text" onChange={onChange} data-consequences="mild2" name="mild2"
                    placeholder="Mild"/>
         </label>
         <label>4
-            <input type="text" onChange={onChange} id="consequence-input4" name="consequence-input4"
+            <input type="text" onChange={onChange} data-consequences="moderate4" name="moderate4"
                    placeholder="Moderate"/>
         </label>
         <label>6
-            <input type="text" onChange={onChange} id="consequence-input6" name="consequence-input6"
+            <input type="text" onChange={onChange} data-consequences="severe6" name="severe6"
                    placeholder="Severe"/>
         </label>
     </div>
